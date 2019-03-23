415e81312b146061d42c1833c48d1acccfc45e54
kacmack <kacmack@wp.pl>
2019-03-23 17:12:58 +0100

    a

---
+++ programowanie strukturalne/3_math_random.py
@@ -0,0 +1,14 @@
+#pi
+import math
+pi = math.pi
+print(pi)
+
+#pierwiastek
+pierwiastek = math.sqrt(9)
+print(pierwiastek)
+print(type(pierwiastek))
+
+#moduł random
+import random
+losuj = random.random()
+print(losuj)
